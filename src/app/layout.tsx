4a543de085fd2bde7c22974860acdc72dced9a81
pejman hkh <pejman.hkh@gmail.com>
2026-01-03 01:32:27 +0330

    feat: add main tag to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="bg-gray-100 min-h-screen">
+          {children}
+        </main>
       </body>
     </html>
   );
