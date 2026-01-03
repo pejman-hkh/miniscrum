b39cc25df25ab8725fd6790ec85a97ebcffdfea5
pejman hkh <pejman.hkh@gmail.com>
2026-01-03 19:11:46 +0330

    feat: add confirmation to DataContext

--- src/context/DataContext.ts
+++ src/context/DataContext.ts
@@ -1,11 +1,14 @@
+import { ConfirmationProps } from "@/components/Confirmation";
 import UserType from "@/types/user";
 import { createContext } from "react";
 
 export interface DataContextType {
     user: UserType | null;
     setUser?: (user: UserType | null) => void;
+    confirmation: ConfirmationProps | null;
+    setConfirmation: (confirmation: ConfirmationProps | null) => void;
 }
 
-const DataContext = createContext<DataContextType>({ user: null });
+const DataContext = createContext<DataContextType>({ user: null, confirmation: null, setConfirmation: () => { } });
 
 export default DataContext;
